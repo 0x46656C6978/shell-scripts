cb31da9c06cac6b5e2e97fae77a8596e67b58750
xileF <nguyenthanhdat.js@gmail.com>
2025-07-01 20:13:26 +0700

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -5,15 +5,15 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install gnome-browser-connector
 
-sh -c “$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)”
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 
-echo “source /home/felix/.gvm/scripts/gvm” >> ~/.zshrc
+echo "source /home/felix/.gvm/scripts/gvm" >> ~/.zshrc
 source ~/.zshrc
 gvm install go1.4 -B
 gvm use go1.4
-echo “export GOROOT_BOOTSTRAP=$GOROOT” >> ~/.zshrc
+echo "export GOROOT_BOOTSTRAP=$GOROOT" >> ~/.zshrc
 source ~/.zshrc
 gvm install go1.7
 gvm use go1.7
@@ -34,5 +34,3 @@ wget https://app.warp.dev/download?package=deb-O warp.deb
 sudo dpkg -i warp.deb
 
 sudo apt install fcitx5 fcitx5-unikey
-
-Đéo hiểu viết lách 

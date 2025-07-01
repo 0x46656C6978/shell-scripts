eac93dd8be1647d6d49126a78af4f03049dd5f35
xileF <nguyenthanhdat.js@gmail.com>
2025-07-01 21:05:07 +0700

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,7 +3,7 @@ sudo apt install -y bison wget curl git zsh make gcc binutils
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt install gnome-browser-connector
+sudo apt install -y gnome-browser-connector
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
